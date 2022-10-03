54b9f75cc747c9734e1d00a2b3bc6cac7d2d44f6
Doretto <dorettoartesp@gmail.com>
2022-10-02 21:13:05 -0300

    deplyment r1

--- react-desafio1/deploy.sh
+++ react-desafio1/deploy.sh
@@ -20,10 +20,8 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:dorettoartesp/ds-ex.git main:gh-pages
 
 cd -
